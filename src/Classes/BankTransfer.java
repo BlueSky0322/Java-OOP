7cd052358809a2e29a494e598f8a48bbfc4d342e
BlueSky0322 <ryanng8628@gmail.com>
2021-11-30 02:21:46 +0800

    added payment, cash, bank transfer classes

--- src/Classes/BankTransfer.java
+++ src/Classes/BankTransfer.java
@@ -5,10 +5,53 @@
  */
 package Classes;
 
+import static Classes.Person.isNumeric;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Ryan Ng
  */
-public class BankTransfer {
+public class BankTransfer extends Payment {
+    private String bank_acc_no;
+    private String bank_name;
     
+    public String getBankAccNo() {
+        return bank_acc_no;
+    }
+
+    public void setBankAccNo(String bank_acc_no) {
+        this.bank_acc_no = bank_acc_no;
+    }
+
+    public String getBankName() {
+        return bank_name;
+    }
+
+    public void setBankName(String bank_name) {
+        this.bank_name = bank_name;
+    }
+    
+    @Override
+    public boolean make_payment() {
+        BufferedWriter bw;
+        try {
+            bw = new BufferedWriter(new FileWriter("Payment.txt", true));
+            bw.write(super.getPassport_no()+ ":"  + super.getPayment_type() + ":" + this.bank_acc_no + ":" + this.bank_name + ":" 
+                    + super.getAmount_paid());
+            bw.write(System.getProperty("line.separator"));
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public boolean validate_amount_paid(){
+        return Payment.AMOUNT == this.amount_paid;
+    }   
 }
--- src/Classes/Payment.java
+++ src/Classes/Payment.java
@@ -5,10 +5,46 @@
  */
 package Classes;
 
+import static Classes.Person.isNumeric;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Ryan Ng
  */
-public class Payment {
+public abstract class Payment {
+    public static final double AMOUNT = 50.00;
+    protected String passport_no;
+    protected String payment_type;
+    protected double amount_paid;
+
+    public String getPassport_no() {
+        return passport_no;
+    }
+
+    public void setPassport_no(String passport_no) {
+        this.passport_no = passport_no;
+    }
     
+    public String getPayment_type() {
+        return payment_type;
+    }
+
+    public void setPayment_type(String payment_type) {
+        this.payment_type = payment_type;
+    }
+    
+    public double getAmount_paid() {
+        return amount_paid;
+    }
+
+    public void setAmount_paid(double amount_paid) {
+        this.amount_paid = amount_paid;
+    }
+    
+    public boolean validate_amount_paid(){
+        return Payment.AMOUNT <= this.amount_paid;
+    }
+    
+    public abstract boolean make_payment();
 }
