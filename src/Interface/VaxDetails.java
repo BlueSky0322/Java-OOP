264e6974a20bb8ac92a5f2876991f29fc76db90c
BlueSky0322 <ryanng8628@gmail.com>
2021-12-01 11:16:20 +0800

    added GenerateID, VaxDetails Interface

---
+++ src/Interface/VaxDetails.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Ryan Ng
+ */
+public interface VaxDetails {
+    static final int FFD_AFTER = 3;
+    static final int MIN_QUANTITY = 100;
+    String setfdd();
+    String setsdd(int a);
+    String setbdd(int a);
+}
